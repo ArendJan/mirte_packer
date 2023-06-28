abbfaeeae758f945fd16090cf120dea5c0cec80c
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2023-06-28 10:20:51 +0200

    git_local fixes

--- packerBuild.sh
+++ packerBuild.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 set -xe
 
 REMOVE_GIT_LOCAL=false
@@ -5,15 +6,24 @@ if [ ! -d git_local ]; then
 REMOVE_GIT_LOCAL=true
 echo "Downloading git stuff"
 mkdir git_local
+fi
+ls git_local > existingFiles.txt
 cd git_local
 # Download all Mirte repositories
 vcs import < ../repos.yaml --workers 1 || true
 cd ..
-fi
+
 PACKER_CONFIG_DIR=$HOME /usr/bin/time -v  sudo -E $(which packer) build .  | tee log.txt
 
-if [ "$REMOVE_GIT_LOCAL" ]; then
+OLD_GLOB=$GLOBIGNORE
+GLOBIGNORE=$(paste -s -d : existingFiles.txt)
+cd git_local
+rm -rf *
+cd ..
+if $REMOVE_GIT_LOCAL; then
 rm -rf git_local
 fi
+GLOBIGNORE=$OLD_GLOB
 
-sudo ./pishrink.sh build/*.img
+sudo ./pishrink.sh build/*.img || true
+rm existingFiles.txt
