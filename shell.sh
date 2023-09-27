95be0dd8c98c5fbd3345d3384d7075f80ae52f06
Arend-Jan <a.j.vanhilten@tudelft.nl>
2023-09-27 15:01:51 +0200

    build script upload fixes

--- shell.sh
+++ shell.sh
@@ -2,5 +2,5 @@
 set -xe
 echo $1
 IMG_FILE=$(readlink -f ./build/"$1") 
-docker build .
-sudo docker run --privileged -v "$IMG_FILE":/mirte_sd.img -it "$(docker build -q .)"
+sudo docker build .
+sudo docker run --privileged -v "$IMG_FILE":/mirte_sd.img -it "$(sudo docker build -q .)"
