359fc2163e70a51171ef13e715795c9b90885cde
Arend-Jan <a.j.vanhilten@tudelft.nl>
2023-11-08 14:56:20 +0100

    change output date format

--- scripts/finalize.sh
+++ scripts/finalize.sh
@@ -5,7 +5,7 @@ finalize() {
 	sudo ./add_partition_local/add_partition.sh $imagefile
 	sudo ./pishrink.sh $imagefile || true
 	filename=$(basename $imagefile .img)
-	newImageFile="build/$filename"_"$(date +"%d-%m-%Y_%H_%M")".img
+	newImageFile="build/$filename"_"$(date +"%Y-%m-%d %H:%M:%S")".img
 	sudo cp "$imagefile" "$newImageFile"
 	xz -T0 --keep -v "$newImageFile" || true
 }
