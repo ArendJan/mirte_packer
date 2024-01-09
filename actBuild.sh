1d383cbc3dacfda17e60e516804fa21b62935800
Arend-Jan <a.j.vanhilten@tudelft.nl>
2024-01-09 17:07:35 +0100

    Add copying artifacts actbuild

--- actBuild.sh
+++ actBuild.sh
@@ -1,1 +1,4 @@
+mkdir /tmp/artifacts || true
 act --container-options="-v /dev:/dev -v /proc:/proc --privileged"  --artifact-server-path /tmp/artifacts
+mkdir artifacts || true
+cp /tmp/artifacts/* ./artifacts
