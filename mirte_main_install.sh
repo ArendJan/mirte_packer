e6d1a7c12d89067732e01f63e3b644745bdaaeb1
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2024-01-09 14:45:41 +0100

    Fix repos

--- mirte_main_install.sh
+++ mirte_main_install.sh
@@ -18,6 +18,7 @@ ls -al
 vcs import --workers 1 --input ./repos.yaml || true
 pip3 install "deepdiff[cli]"
 deep diff --ignore-order --ignore-string-case ./repos.yaml ./mirte-install-scripts/repos.yaml # to show the difference between the repos.yaml in here and in mirte-install-scripts/repos.yaml
+cp ./repos.yaml ./mirte-install-scripts/repos.yaml # overwrite the repos.yaml in mirte-install-scripts with the one in here
 ls -al
 cd /usr/local/src/mirte/mirte-install-scripts/ && ./create_user.sh
 echo 'mirte ALL=(ALL) NOPASSWD:ALL' >>/etc/sudoers
